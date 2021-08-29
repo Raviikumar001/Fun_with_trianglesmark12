22f9b724101491ca9470481eb588b1783cc26963
Raviikumar001 <samcollins9899@gmail.com>
2021-08-30 01:25:44 +0530

    added logic for hypotenuse

--- script/hypotenuse.js
+++ script/hypotenuse.js
@@ -1,19 +1,20 @@
 const sides = document.querySelectorAll(".side-input");
 const calculateBtn = document.querySelector("#calculate-hypotenuse-btn");
-const outputEl = document.querySelector(".output");
-
+const msg = document.querySelector("#msg");
+console.log("hi");
 function calculateSumOfSquare(a, b) {
   const sumOfSquares = a * a + b * b;
   return sumOfSquares;
 }
 
 function calculateHypotenuse() {
+  
   const sumOfSquares = calculateSumOfSquare(
     Number(sides[0].value),
     Number(sides[1].value)
   );
   const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-  outputEl.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
+  msg.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
 }
 
 calculateBtn.addEventListener("click", calculateHypotenuse);
