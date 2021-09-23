d66cd7984cef6231f6c59caffb15b145a1ca182b
Raviikumar001 <52815192+Raviikumar001@users.noreply.github.com>
2021-09-23 15:40:19 +0530

    updated the hypotenuse logic

--- script/hypotenuse.js
+++ script/hypotenuse.js
@@ -1,20 +1,32 @@
-const sides = document.querySelectorAll(".side-input");
+const side1 = document.querySelector(".side-input1");
+const side2 = document.querySelector(".side-input2");
 const calculateBtn = document.querySelector("#calculate-hypotenuse-btn");
 const msg = document.querySelector("#msg");
-console.log("hi");
+
 function calculateSumOfSquare(a, b) {
   const sumOfSquares = a * a + b * b;
   return sumOfSquares;
 }
 
 function calculateHypotenuse() {
+  console.log("hi");
+   first=Number(side1.value);
+   second=Number(side2.value);
+   console.log(first);
+  if((first<=0) || (second<=0))
+  {
+    msg.innerText="Pls enter positive values.";
+    return
+  }
+  else{
   
   const sumOfSquares = calculateSumOfSquare(
-    Number(sides[0].value),
-    Number(sides[1].value)
+    Number(side1.value),
+    Number(side2.value)
   );
   const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
   msg.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
+  }
 }
 
 calculateBtn.addEventListener("click", calculateHypotenuse);
